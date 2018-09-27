f7992fa95c26c773f50e83a6aa12776eaaa28aa9
Gerson Barbosa <gersontiger@gmail.com>
2018-09-27 15:09:54 -0300

    Acrescentada a mensagem de despedida

--- projeto-teste/Program.cs
+++ projeto-teste/Program.cs
@@ -9,7 +9,7 @@ namespace projeto_teste
             Console.WriteLine("Olá Mundo!");          
             Console.WriteLine("Como você está hoje?");
             Console.WriteLine("Tenha um bom dia!");
-
+            Console.WriteLine("Tchau!");
             Console.ReadLine();
         }
     }
